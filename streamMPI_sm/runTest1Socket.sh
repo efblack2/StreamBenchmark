b2ce1f3c5e68fbf808deb10abe86dbc317d4d8ea
Edgar F. Black <efblack2@illinois.edu>
2018-03-07 16:58:36 -0600

    modidied script to allow gather data from copy, add, triad and scale tests

--- streamMPI_sm/runTest1Socket.sh
+++ streamMPI_sm/runTest1Socket.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-if [ "$#" -ne 1 ] 
+if [ "$#" -lt 1 ] 
 then
-  echo "Usage: $0 compilerName"
+  echo "Usage: $0 compilerName [Copy | Scale | Add | Triad]"
   exit 1
+fi
+
+if [ -z "$2" ]; then
+    test='Copy'
+else
+    test=$2
 fi
 
 npt=`grep -c ^processor /proc/cpuinfo`
@@ -19,8 +25,8 @@ done
 
 mkdir -p ../../plots/StreamCopyResults/$(hostname)-1Socket/$1
 
-cat temp.txt | grep "Copy:\|counted" | awk ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == "Copy:")rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamCopyResults/$(hostname)-1Socket/$1/streamMPI_sm.dat
+
+cat temp.txt | grep "$test:\|counted" | awk -v myTest=$test: ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == myTest)rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > > ../../plots/StreamCopyResults/$(hostname)-1Socket/$1/streamMPI_sm_$test.dat
 
 rm temp.txt
-
 
--- streamOpenMP/runTest.sh
+++ streamOpenMP/runTest.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-if [ "$#" -ne 1 ] 
+if [ "$#" -lt 1 ] 
 then
-  echo "Usage: $0 compilerName"
+  echo "Usage: $0 compilerName [Copy | Scale | Add | Triad]"
   exit 1
+fi
+
+if [ -z "$2" ]; then
+    test='Copy'
+else
+    test=$2
 fi
 
 npt=`grep -c ^processor /proc/cpuinfo`
 np=$npt
 #np="$(($npt / 2))"
 rm -f temp.txt
-
 for i in  `seq 1 $np`; do
     echo number of threads: $i
     export OMP_NUM_THREADS=$i
@@ -18,7 +23,7 @@ done
 
 mkdir -p ../../plots/StreamCopyResults/$(hostname)/$1
 
-cat temp.txt | grep "Copy:\|counted" | awk ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == "Copy:")rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamCopyResults/$(hostname)/$1/streamOpenMP.dat
+cat temp.txt | grep "$test:\|counted" | awk  -v myTest=$test: ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == myTest)rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamCopyResults/$(hostname)/$1/streamOpenMP_$test.dat
 
 rm temp.txt
 
