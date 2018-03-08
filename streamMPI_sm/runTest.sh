29f261302165e4a8a9fded259dc18ba129ccfdbb
Edgar F. Black <efblack2@illinois.edu>
2018-03-08 11:19:28 -0600

    fixing running scripts

--- streamMPI_sm/runTest.sh
+++ streamMPI_sm/runTest.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 if [ "$#" -lt 1 ] 
 then
-  echo "Usage: $0 compilerName [Copy | Scale | Add | Triad]"
+  echo "Usage: $0 compilerName"
   exit 1
-fi
-
-if [ -z "$2" ]; then
-    test='Copy'
-else
-    test=$2
 fi
 
 npt=`grep -c ^processor /proc/cpuinfo`
@@ -21,10 +15,17 @@ for i in  `seq 1 $np`; do
     mpiexec -n $i streamMPI_sm >> temp.txt
 done
 
-mkdir -p ../../plots/StreamCopyResults/$(hostname)/$1
+mkdir -p ../../plots/StreamResults/$(hostname)/$1
 
-cat temp.txt | grep "$test:\|counted" | awk -v myTest=$test: ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == myTest)rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamCopyResults/$(hostname)/$1/streamMPI_sm_$test.dat
+cat temp.txt | grep "Copy:\|counted" | awk ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == "Copy:")rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamResults/$(hostname)/$1/streamMPI_sm_Copy.dat
 
-rm temp.txt
+cat temp.txt | grep "Scale:\|counted" | awk ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == "Scale:")rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamResults/$(hostname)/$1/streamMPI_sm_Scale.dat
+
+cat temp.txt | grep "Add:\|counted" | awk ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == "Add:")rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamResults/$(hostname)/$1/streamMPI_sm_Add.dat
+
+cat temp.txt | grep "Triad:\|counted" | awk ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == "Triad:")rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamResults/$(hostname)/$1/streamMPI_sm_Triad.dat
+
+
+#rm temp.txt
 
 
--- streamMPI_sm/runTest1Socket.sh
+++ streamMPI_sm/runTest1Socket.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 if [ "$#" -lt 1 ] 
 then
-  echo "Usage: $0 compilerName [Copy | Scale | Add | Triad]"
+  echo "Usage: $0 compilerName"
   exit 1
-fi
-
-if [ -z "$2" ]; then
-    test='Copy'
-else
-    test=$2
 fi
 
 npt=`grep -c ^processor /proc/cpuinfo`
@@ -23,10 +17,16 @@ for i in  `seq 1 $np`; do
     mpiexec -n $i taskset -c 0-$npm1 streamMPI_sm >> temp.txt
 done
 
-mkdir -p ../../plots/StreamCopyResults/$(hostname)-1Socket/$1
+mkdir -p ../../plots/StreamResults/$(hostname)-1Socket/$1
 
 
-cat temp.txt | grep "$test:\|counted" | awk -v myTest=$test: ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == myTest)rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamCopyResults/$(hostname)-1Socket/$1/streamMPI_sm_$test.dat
+cat temp.txt | grep "Copy:\|counted" | awk ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == "Copy:")rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamResults/$(hostname)-1Socket/$1/streamMPI_sm_Copy.dat
+
+cat temp.txt | grep "Scale:\|counted" | awk ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == "Scale:")rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamResults/$(hostname)-1Socket/$1/streamMPI_sm_Scale.dat
+
+cat temp.txt | grep "Add:\|counted" | awk ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == "Add:")rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamResults/$(hostname)-1Socket/$1/streamMPI_sm_Add.dat
+
+cat temp.txt | grep "Triad:\|counted" | awk ' BEGIN {i=0;j=0}   { if ($4 == "counted") np[i++]=$6;  if ($1 == "Triad:")rate[j++]=$2;}   END {for (j=0; j<i; ++j) printf("%d %.1f\n",  np[j], rate[j])}' > ../../plots/StreamResults/$(hostname)-1Socket/$1/streamMPI_sm_Triad.dat
 
 rm temp.txt
 
